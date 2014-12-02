5bce2689b6fd0a92b5cde7ce250960dcf2fbda0d
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-12-02 13:20:07 +0100

    Update the base strategy to not depend on anything from drugs

--- Change/src/Classes/AvailabilityStrategy.java
+++ Change/src/Classes/AvailabilityStrategy.java
@@ -12,13 +12,19 @@ import java.util.Random;
  *
  * @author alexandersteen
  */
-public class AvailabilityStrategy implements AvailabilityStrategyInterface{
+public class AvailabilityStrategy implements AvailabilityStrategyInterface {
+
+    private final int baseAvailability;
+
+    public AvailabilityStrategy(int baseAvailability) {
+        this.baseAvailability = baseAvailability;
+    }
 
     @Override
     public int calculateAvailability(int baseAvailability) {
         Random r = new Random();
         int factor = 1;
-        switch(r.nextInt(2)){
+        switch (r.nextInt(2)) {
             case 0:
                 factor = 1;
                 break;
@@ -26,10 +32,10 @@ public class AvailabilityStrategy implements AvailabilityStrategyInterface{
                 factor = -1;
                 break;
         }
-        
-        int precent = r.nextInt(41)+15;
-        int change = (Math.round((baseAvailability * precent) / 100)) * factor;
-        return baseAvailability + change;
+
+        int precent = r.nextInt(41) + 15;
+        int change = (Math.round((this.baseAvailability * precent) / 100)) * factor;
+        return this.baseAvailability + change;
     }
-    
+
 }
--- Change/src/Classes/PriceStrategy.java
+++ Change/src/Classes/PriceStrategy.java
@@ -12,13 +12,20 @@ import java.util.Random;
  *
  * @author alexandersteen
  */
-public class PriceStrategy implements PriceStrategyInterface{
+public class PriceStrategy implements PriceStrategyInterface {
+
+    private final int basePrice;
+    private int lastPrice;
+
+    public PriceStrategy(int basePrice) {
+        this.basePrice = this.lastPrice = basePrice;
+    }
 
     @Override
     public int calculatePrice(int basePrice, int goldenNumber) {
         Random r = new Random();
         int factor = 1;
-        switch(r.nextInt(2)){
+        switch (r.nextInt(2)) {
             case 0:
                 factor = 1;
                 break;
@@ -27,16 +34,19 @@ public class PriceStrategy implements PriceStrategyInterface{
                 break;
         }
 
-        if(goldenNumber >= r.nextInt(99)+1){
-            int finalPrice = basePrice * 10 * factor;
-            if(finalPrice < 1)
+        if (goldenNumber >= r.nextInt(99) + 1) {
+            int finalPrice = this.lastPrice * 10 * factor;
+            if (finalPrice < 1) {
                 finalPrice = 1;
-            return finalPrice;
-        }else{
-            int precent = r.nextInt(85)+1;
-            int change = (Math.round((basePrice * precent) / 100)) * factor;
-            return basePrice + change;
+            }
+            this.lastPrice = finalPrice;
+        } else {
+            int precent = r.nextInt(85) + 1;
+            int change = (Math.round((this.lastPrice * precent) / 100)) * factor;
+            this.lastPrice = this.lastPrice + change;
         }
+
+        return this.lastPrice;
     }
-    
+
 }
