79f00ffd14a58cee7a4a861969a6a9ba32048147
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-12-02 13:09:49 +0100

    Remove sout

--- Change/src/Classes/TenPercentPriceStrategy.java
+++ Change/src/Classes/TenPercentPriceStrategy.java
@@ -10,7 +10,6 @@ public class TenPercentPriceStrategy implements PriceStrategyInterface {
 
     @Override
     public int calculatePrice(int basePrice, int goldenNumber) {
-        System.out.println(START_PRICE + "--" + days );
         return (int) (START_PRICE * (1 + (0.1 * days++)));
     }
 
