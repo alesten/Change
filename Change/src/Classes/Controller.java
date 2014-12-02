18c2dd1f592514e8b3d6ea8bee86705af7385c4e
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-12-02 13:47:47 +0100

    Weed should use PickOneOf10 strategy

--- Change/src/Classes/Controller.java
+++ Change/src/Classes/Controller.java
@@ -23,51 +23,51 @@ import java.util.Map;
  * @author alexandersteen
  */
 public class Controller implements ControllerInterface {
-
+    
     private final HighscoreRepository highscoreRepository;
     private ArrayList<CountryInterface> countries;
     private CountryInterface startCountry;
     private Player player;
     private int days;
     private List<EventInterface> events;
-
+    
     public Controller(String name) {
         this.highscoreRepository = new HighscoreRepository("highscores.csv");
         this.countries = new ArrayList();
-
+        
         CountryInterface denmark = new Country("Denmark", getDrugs());
         CountryInterface columbia = new Country("Columbia", getDrugs());
         CountryInterface germany = new Country("Germany", getDrugs());
         CountryInterface usa = new Country("USA", getDrugs());
         CountryInterface france = new Country("France", getDrugs());
         CountryInterface afghanistan = new Country("Afghanistan", getDrugs());
-
+        
         countries.add(denmark);
         countries.add(columbia);
         countries.add(germany);
         countries.add(usa);
         countries.add(france);
         countries.add(afghanistan);
-
+        
         startCountry = denmark;
-
+        
         this.player = new Player(name, 5000, 100, startCountry);
         this.days = 0;
-
+        
         events = new ArrayList<>();
-
+        
         EventInterface customs = new CustomsEvent();
         EventInterface pusher = new PusherEvent();
-
+        
         events.add(customs);
         events.add(pusher);
     }
-
+    
     @Override
     public int getElapsedDays() {
         return days;
     }
-
+    
     @Override
     public List<EventInterface> fly(CountryInterface country) {
         List<EventInterface> encounteredEvents = new ArrayList<>();
@@ -75,7 +75,7 @@ public class Controller implements ControllerInterface {
         player.setCurrentCountry(country);
         shakeContries();
         days++;
-
+        
         for (EventInterface event : events) {
             if (event.doesEventHappen(playerServices)) {
                 encounteredEvents.add(event);
@@ -84,48 +84,48 @@ public class Controller implements ControllerInterface {
                 event.eventAction(player);
             }
         }
-
+        
         return encounteredEvents;
     }
-
+    
     @Override
     public List<CountryInterface> getCountries() {
         return countries;
     }
-
+    
     @Override
     public PlayerInterface getPlayer() {
         return player;
     }
-
+    
     @Override
     public boolean vistHospital(PlayerInterface player) {
         if (player.getLife() >= 100) {
             return false;
         }
-
+        
         player.withdraw(player.getBalance() / 4);
         player.grainLife(100 - player.getLife());
         return true;
-
+        
     }
-
+    
     private void shakeContries() {
         for (CountryInterface country : countries) {
             country.getMarketplace().shakeMarket();
         }
     }
-
+    
     @Override
     public List<HighscoreItemInterface> getHighscores() {
         return this.highscoreRepository.fetch();
     }
-
+    
     @Override
     public void resetGame() {
-
+        
     }
-
+    
     @Override
     public void endGame() {
         // Sell the players drugs
@@ -137,11 +137,11 @@ public class Controller implements ControllerInterface {
         // Save the score to the highscore
         this.highscoreRepository.insert(new HighscoreItem(player.getName(), player.getBalance()));
     }
-
+    
     private DrugInterface[] getDrugs() {
         AvailabilityStrategyInterface basicAvailabilityStrategy = new BaseAvailabilityStrategy(10);
         PriceStrategyInterface basicPriceStrategy = new BasePriceStrategy(1000);
-
+        
         return new DrugInterface[]{
             new Drug("Cocaine", basicPriceStrategy, basicAvailabilityStrategy, 10),
             new Drug("Heroin", new BasePriceStrategy(1600), new BaseAvailabilityStrategy(15), 15),
@@ -150,7 +150,7 @@ public class Controller implements ControllerInterface {
             new Drug("Angel Dust", new TenPercentPriceStrategy(), new TenPercentAvailabilityStrategy(), 7),
             new Drug("Crystal Meth", new BasePriceStrategy(800), new BaseAvailabilityStrategy(38), 12),
             new Drug("Hash", basicPriceStrategy, basicAvailabilityStrategy, 4),
-            new Drug("Weed", basicPriceStrategy, basicAvailabilityStrategy, 5),
+            new Drug("Weed", new PickOneOf10PriceStrategy(new int[]{150, 230, 180, 2350, 17, 360, 190, 440, 550}), new PickOneOf10AvailabilityStrategy(new int[]{100, 190, 200000, 2, 95, 30, 165, 185, 250}), 5),
             new Drug("Mushrooms", basicPriceStrategy, basicAvailabilityStrategy, 7),
             new Drug("Valium", new BasePriceStrategy(290), new BaseAvailabilityStrategy(80), 7)
         };
